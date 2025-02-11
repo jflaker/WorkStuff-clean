00a81b928fe41a0391ef3ea95bff42481f575285
Jeffrey Flaker <jflaker@users.noreply.github.com>
2025-02-11 13:53:10 -0600

    Add files via upload

--- Scripts/Start PythonWebServer.py
+++ Scripts/Start PythonWebServer.py
@@ -7,3 +7,13 @@ os.chdir(r'C:\Users\REDACTED-HOST\Desktop\SlideShowExperiment')
 
 # Open a new command prompt window, run the http.server command on port 80, then close the window upon termination
 subprocess.Popen(['start', 'cmd', '/k', 'python -m http.server 80 ^& exit'], shell=True)
+
+
+import os
+import subprocess
+
+# Change the working directory
+os.chdir(r'C:\Users\REDACTED-HOST\Desktop\SlideShowExperiment')
+
+# Open a new command prompt window, run the http.server command on port 80, then close the window upon termination
+subprocess.Popen(['cmd', '/c', 'start', 'cmd', '/k', 'python -m http.server 80 ^& exit'], shell=True)
